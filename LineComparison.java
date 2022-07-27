1025b54d6c5a26e31821f0858349a8c95959800d
rutuja_mali <rutujamali111@gmail.com>
2022-07-27 23:05:59 +0530

    Equality of length

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class LineComparison {
-	static Scanner sc=new Scanner(System.in);
-	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	double LengthOfLine()
+	{
 		System.out.println("Enter starting co-ordinates of line");
 		int x1=sc.nextInt();
 		 int y1=sc.nextInt();
@@ -10,8 +11,21 @@ public class LineComparison {
 		 int x2=sc.nextInt();
 		 int y2=sc.nextInt();
 		double length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		System.out.println("Length of line is: "+length);
+		return length;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LineComparison line=new LineComparison();
+		double length_of_line_1=line.LengthOfLine();
+		double length_of_line_2=line.LengthOfLine();
+		if(length_of_line_1==length_of_line_2)
+			System.out.println("Two lines are equal...");
+		else 
+			System.out.println("not euals");
+		
 	}
 
 }
+
 
